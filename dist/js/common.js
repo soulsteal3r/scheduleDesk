f247d6bc7c35e2427814362b84a96ddee432cf13
Alex <homahomchikov@gmail.com>
2018-10-04 18:39:49 +0300

    Hamb menu
    Bubble menu & hamb menu animation;

--- dist/js/common.js
+++ dist/js/common.js
@@ -1,17 +1,29 @@
-var bubbleTrigger = document.getElementById("bubbleTrigger");
-var bubblebg = document.getElementById("bubbleBg");
-var bubblemenu = document.getElementById("bubbleMenu");
 var body = document.getElementById("body");
 
+//Bubble menu <=575
+var bubbleTrigger = document.getElementById("bubbleTrigger");
+var bubbleBg = document.getElementById("bubbleBg");
+var bubbleMenu = document.getElementById("bubbleMenu");
+
+//Hamb menu  >= 576 & <= 991
+var hambTrigger = document.getElementById("hambTrigger");
+var hambMenu = document.getElementById("hambMenu");
+
 bubbleTrigger.onclick = function(){
-  if(bubblebg.classList.contains("active")){
-    bubblebg.style.transition = ".6s";
+  if(bubbleBg.classList.contains("active")){
+    bubbleBg.style.transition = ".6s";
   }else{
-    bubblebg.style.transition = "1s";
+    bubbleBg.style.transition = "1s";
   }
-  bubblebg.classList.toggle("active");
+  bubbleBg.classList.toggle("active");
+  bubbleTrigger.classList.toggle("active");
   body.classList.toggle("ovh");
   setTimeout(function(){
-    bubblemenu.classList.toggle("active");
+    bubbleMenu.classList.toggle("active");
   }, 200);
+}
+
+hambTrigger.onclick = function(){
+  hambTrigger.classList.toggle("active");
+  hambMenu.classList.toggle("active");
 }
